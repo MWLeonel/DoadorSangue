89b8a3de5d548f388624eb1508e62e19e4b97592
MWLeonel <wmleonel6@gmail.com>
2020-10-01 13:30:05 -0300

    Campanha do Sangue

---
+++ DoadorSangue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.Write("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade <= 17)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Você não pode ser doador de sangue");
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+            if (idade >= 18 && idade <= 67)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Você pode ser doador de sangue");
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+            if (idade >= 68)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Você não pode ser doador de sangue");
+                Console.WriteLine("Pressione ENTER para sair!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
